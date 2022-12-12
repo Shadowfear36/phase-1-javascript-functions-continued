9a2b6bba585d1a34ca9dd68cbe8567e73bbcc113
DylanRhinehart <dylanrhinehart@gmail.com>
2022-12-12 13:33:48 -0800

    commit tree a566abc33a976f11a02e43d134e5c09343823470

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(input = 'roller-skate'){
+    let target = input;
+    return(`This Saturday, I want to ${target}!`)
+};
+
+function mondayWork(input = `go to the office`){
+    let target = input;
+    return(`This Monday, I will ${target}.`);
+
+}
+function wrapAdjective(icon ="*"){
+    return function (adjective) {
+        return `You are ${icon}${adjective}${icon}!`;
+    }
+
+};
